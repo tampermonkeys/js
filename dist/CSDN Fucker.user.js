beddcc257986a1e56ce1e1fe8b544d92ddf0957b
おばさんは铁道を洗います <@tampermonkeys>
2019-05-21 17:38:57 +0800

    remove jq

--- dist/CSDN Fucker.user.js
+++ dist/CSDN Fucker.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         CSDN Fucker
 // @namespace    https://github.com/tampermonkeys/tampermonkey/
-// @version      0.1
+// @version      0.11
 // @description  try to take over the world!
 // @author       おばさんは铁道を洗います
 // @match        *://blog.csdn.net/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/jquery@3.2.1/src/core.min.js
 // ==/UserScript==
 
 (function() {
